61ac9e6159f99300f2cae6f01452199c9cdac19e
cpbryant <113325662+cpbryant@users.noreply.github.com>
2024-02-03 12:41:57 -0500

    backend changes

---
+++ backend/src/Parser.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Parser {
+
+  public Reader reader;
+  public Parser(Reader reader){
+    this.reader = reader;
+  }
+}
